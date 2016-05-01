571fe082460711f14f181812affa1530365da28d
iankit3 <a4ankit@outlook.com>
2016-05-01 17:51:33 +0530

    added hackernews app

---
+++ hackernews/js/script.js
@@ -0,0 +1,27 @@
+angular.module("myApp",[])
+       
+ 
+
+ .controller("newsController",['$scope','$http', function($scope,$http){
+ 
+ $scope.myData = [];   
+
+  $http({
+     method:'GET',
+     url:'http://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+  }).success(function(data){
+    console.log(data);
+    data = data.splice(0,10);
+    console.log(data);
+    data.forEach(function(e,i){
+          $http({
+             method:'GET',
+             url:'https://hacker-news.firebaseio.com/v0/item/'+ e +'.json?print=pretty'
+           }).success(function(d){
+               ($scope.myData).push(d);
+             })
+     })   
+ })   
+          $scope.text = "Lehsoon";
+          console.log($scope.myData);
+       }])

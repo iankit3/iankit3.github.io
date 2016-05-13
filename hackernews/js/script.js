ab18370ae4ffe1fabf30c0ff4cfcd12e1d188065
iankit3 <a4ankit@outlook.com>
2016-05-13 18:04:18 +0530

    CORS

--- hackernews/js/script.js
+++ hackernews/js/script.js
@@ -1,12 +1,13 @@
 var app = angular.module("myApp",[])
-
-app.config(function($httpProvider) {
+       
+ app.config(function($httpProvider) {
       //Enable cross domain calls
       $httpProvider.defaults.useXDomain = true;
  
       //Remove the header used to identify ajax call  that would prevent CORS from working
       delete $httpProvider.defaults.headers.common['X-Requested-With'];
   });
+
 app.controller("newsController",['$scope','$http', function($scope,$http){
  
  $scope.myData = [];   

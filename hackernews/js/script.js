3e5ba147189fc521f7fa11c7c67e18ba552cc7ff
iankit3 <a4ankit@outlook.com>
2016-05-25 13:37:23 +0530

    more

--- hackernews/js/script.js
+++ hackernews/js/script.js
@@ -1,37 +1,36 @@
-var app = angular.module("myApp",[])
+var app = angular.module("myApp", [])
 app.config(function($httpProvider) {
-      //Enable cross domain calls
-      $httpProvider.defaults.useXDomain = true;
- 
-      //Remove the header used to identify ajax call  that would prevent CORS from working
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  });       
-app.controller("newsController",['$scope','$http', function($scope,$http){
- 
- $scope.myData = [];   
-$scope.getMore = function(){
-var S = 0,E = 10 ;
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
 
-  $http({
-     method:'GET',
-     url:'http://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
-  }).success(function(data){
-    console.log(data);
-    data = data.splice(S,E);
-    console.log(data);
-    data.forEach(function(e,i){
-          $http({
-             method:'GET',
-             url:'https://hacker-news.firebaseio.com/v0/item/'+ e +'.json?print=pretty'
-           }).success(function(d){
-               ($scope.myData).push(d);
-             })
-     })   
- })   
-          $scope.text = "Lehsoon";
-          console.log($scope.myData);
+    //Remove the header used to identify ajax call  that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+app.controller("newsController", ['$scope', '$http', function($scope, $http) {
+    $scope.myData = [];
+    var S = 0,
+        E = 5;
+    $scope.getMore = function() {
+        $http({
+            method: 'GET',
+            url: 'http://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+        }).success(function(data) {
+            console.log(data);
+            data = data.slice(S,E);
+            console.log(data);
+            data.forEach(function(e, i) {
+                $http({
+                    method: 'GET',
+                    url: 'https://hacker-news.firebaseio.com/v0/item/' + e + '.json?print=pretty'
+                }).success(function(d) {
+                    ($scope.myData).push(d);
+                })
+            })
+        })
+        $scope.text = "Lehsoon";
+        console.log($scope.myData);
 
-S = E;
-E += 10;
-}
-       }])
+        S = E;
+        E += 5;
+    }
+}])

d6d6b42467fffaa702480675569717cf8ea25692
iankit3 <a4ankit@outlook.com>
2016-06-28 13:46:26 +0530

    the local version

--- hackernews/js/script.js
+++ hackernews/js/script.js
@@ -6,8 +6,22 @@ app.config(function($httpProvider) {
     //Remove the header used to identify ajax call  that would prevent CORS from working
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
+
+function epocToDate(epoc){
+ var myDate = new Date(epoc*1000);
+ var dateF = (myDate.toLocaleString()).slice(0,4).replace('/','-') 
+             + (myDate.toLocaleString()).slice(10,15) 
+             +" "+ (myDate.toLocaleString()).slice(19,22) 
+     myDate =  myDate.toLocaleString();
+   return dateF   
+}
 app.controller("newsController", ['$scope', '$http', function($scope, $http) {
     $scope.myData = [];
+    $scope.showUrl = function(index,e){
+      if(e.show)e.show = false;
+      else e.show = true;
+    }
+
     var S = 0,
         E = 5;
     $scope.getMore = function() {
@@ -23,6 +37,8 @@ app.controller("newsController", ['$scope', '$http', function($scope, $http) {
                     method: 'GET',
                     url: 'https://hacker-news.firebaseio.com/v0/item/' + e + '.json'
                 }).success(function(d) {
+                    d.show = false;
+                    d.time = epocToDate(d.time);
                     ($scope.myData).push(d);
                 })
             })
@@ -33,4 +49,5 @@ app.controller("newsController", ['$scope', '$http', function($scope, $http) {
         S = E;
         E += 5;
     }
+
 }])

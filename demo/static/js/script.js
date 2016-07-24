32c23d5581b75493ab4b0810adca2e7af266062b
iankit3 <a4ankit@outlook.com>
2016-07-24 21:17:48 +0530

    just temp2

--- demo/static/js/script.js
+++ demo/static/js/script.js
@@ -1,8 +1,15 @@
 $(function(){
+
+	$(function(){
+	    $('.carousel').carousel({
+	        interval: 2000
+	    });
+	}())
+	
 	$(".nav.navbar-nav li").click(function(){
 	   $(".nav.navbar-nav li").removeClass('active')
 	   if(!$(this).hasClass('active')){
 	      $(this).addClass('active')
 	    }
 	})
-}())
+})
